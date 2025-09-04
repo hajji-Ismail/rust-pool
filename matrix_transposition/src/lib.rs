4336936ae27e326b7397bb7457169ec0cb571cb4
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-04 16:02:25 +0100

    matrix1

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,21 +1,10 @@
 #[derive(Debug)]
-pub struct Matrix {
-    row: [i32; 2],
-    col: [i32; 2],
-}
+pub struct Matrix (
+    pub (i32 , i32),
+    pub (i32 , i32)
 
+);
 pub fn transpose(m: Matrix) -> Matrix {
-    let mut matrix = Matrix {
-        row : [0,0],
-        col : [0,0]
-
-    }; 
-    let length = matrix.col.len();
-    for i in 0..length-1 {
-        matrix.row[i] = m.col[i];
-        matrix.col[i]= m.row[i];
-
-
-    }
-    matrix
+    
+    return Matrix((m.0.0, m.1.0),(m.0.1, m.1.1))
 }
