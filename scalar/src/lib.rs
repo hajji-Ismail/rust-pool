3680e15ef5bd08f894e9ee86d824c3972db2f397
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-04 10:55:54 +0100

    add git ignore

---
+++ scalar/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn sum(left: u8, right: u8) -> u8 {
+    left + right
+}
+
+pub fn diff(left: i16, right: i16) -> i16 {
+    left - right
+}
+pub fn pro(left: i8, right: i8) -> i8 {
+    left * right
+}
+
+pub fn quo(left: f32, right: f32) -> f32 {
+    left / right
+}
+
+pub fn rem(left: f32, right: f32) -> f32 {
+    left % right
+}
