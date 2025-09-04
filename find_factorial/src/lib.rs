3adf71ecb96ca0a90ed6662c511857c23c99258c
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-04 13:46:48 +0100

    finxing the func

--- find_factorial/src/lib.rs
+++ find_factorial/src/lib.rs
@@ -2,7 +2,7 @@ pub fn factorial(num: u64) -> u64 {
     if num == 1 || num == 0 {
         return 1;
     }
-    let mut  res = 1 ;
+    let mut  res = num ;
     for i in 1..num {
         res = res * i;
 
