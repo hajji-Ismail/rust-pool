c94bfff17f7520b22bd7d77943fe754ab58bd3bf
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-05 12:47:55 +0100

    copy

---
+++ copy/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn nbr_function(c: i32) -> (i32, f64, f64) {
+    let x : f64 = c as f64;
+    (c,x.exp(), x.abs().ln())
+}
+
+pub fn str_function(a: String) -> (String, String) {
+    let mut res : String = Default::default() ;
+for item in a.split(" "){
+    let mut conv = item.parse::<f64>().unwrap();
+    let  nla = conv.exp().to_string();
+    res.push_str(&(nla + " "))
+
+}
+
+    (a,res)
+}
+
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+    let mut res:Vec<f64> = vec![];
+    for item in &b {
+        let conv: f64 = *item as f64;
+        res.push(conv.abs().ln())
+
+    }
+    (b,res)
+}
