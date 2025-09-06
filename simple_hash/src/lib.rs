63a6f35baee727c9b75137ec4cee0f5e1982dbf2
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-06 14:43:12 +0100

    testing

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,18 @@
+ use std::collections::HashMap;
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    let mut res = HashMap::new();
+    for word in words {
+        if res.contains_key(word){
+            let old = res.get(word).unwrap();
+            res.insert(*word, old+1);
+        } else {
+            res.insert(word, 1);
+        }
+    }
+    res
+
+}
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    1
+}
