a1b8832f0ad810854bbd0bc08b8ada008aec28f4
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-06 11:33:13 +0100

    arrays

---
+++ arrays/src/main.rs
@@ -0,0 +1,14 @@
+use arrays::*;
+
+fn main() {
+
+    let b = [5;10];
+
+
+    println!("The sum of the elements in {:?} is {}", b, sum(&b));
+    println!(
+        "Array of {} elements filled with 10 = {:?}",
+        thirtytwo_tens().len(),
+        thirtytwo_tens()
+    );
+}
