773bbf980386076af7c7c25e07106a329dd83d08
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-06 13:19:29 +0100

    editing

---
+++ edit_distance/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn edit_distance(source: &str, target: &str) -> usize {
+    let mut res : usize = 0;
+    if source < target {
+        res += target.len()- source.len()
+    } else {
+        res += source.len()- target.len()
+    }
+    let mut plus = false;
+    for c1 in source.chars() {
+        for c2 in target.chars(){
+           
+            if  c1 == c2 {
+                plus = true;
+            }
+
+        }
+        if plus == false {
+            res += 1;
+        }else {
+            plus = false
+        }
+    }
+
+
+
+    res
+
+}
