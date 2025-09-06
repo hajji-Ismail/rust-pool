f73624276996fcb2f4e2b7ae23b76208987b4a56
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-06 16:46:31 +0100

    bubble sort

---
+++ collect/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn bubble_sort(arr: &mut [i32]) {
+    for i in 0.. arr.len(){
+        for j in 0..arr.len()-i {
+            if arr[j] > arr[i]{
+                arr.swap(i,j)
+            }
+        }
+    }
+}
