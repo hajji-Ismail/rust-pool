81133720c571d610338f1e76df8b6b4f5090695d
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-06 09:46:18 +0100

    changes

---
+++ changes/src/lib.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Light {
+	pub alias: String,
+	pub brightness: u8,
+}
+
+impl Light {
+	pub fn new(alias: &str) -> Self {
+        Light { alias: alias.to_string(), brightness: 0 }
+	}
+}
+
+pub fn change_brightness(lights: &mut [Light], alias: &str, value: u8) {
+	for i in 0..lights.len() {
+		if lights[i].alias == alias {
+			lights[i].brightness = value
+		}
+	}
+
+}
