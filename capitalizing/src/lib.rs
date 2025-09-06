d63f5057c6fcbcac532ee76b918d91fa6e404d69
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-06 12:50:42 +0100

    implementing new logic to it

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -10,19 +10,37 @@ pub fn capitalize_first(input: &str) -> String {
 
 pub fn title_case(input: &str) -> String {
     let mut res : String = "".to_string();
+    let mut can = true ;
     let  words = input.split(" ");
-    for i in words {
-        let  sub : String;
-        let  chars :  Vec<char> = i.chars().collect();
-         let f = chars[0].to_uppercase();
-        sub = f.to_string()+&chars[1..].iter().collect::<String>();
-        res.push_str(&sub);
-        res.push(' ');
+    for word in words {
+        for c in word.chars() {
+            if c.is_whitespace() {
+                res.push(c);
+                can = true ;
 
+            } else if can {
+                 let f = c.to_uppercase();
+                 res = res + & f.to_string();
+                 can = false;
+            } else {
+              res.push(c);
+            }
+            
+        }
+        res.push(' ');
+        can = true ;
+
+        
     }
     res.trim().to_string()
- 
+    
 }
+// let  sub : String;
+// let  chars :  Vec<char> = i.chars().collect();
+//  let f = chars[0].to_uppercase();
+// sub = f.to_string()+&chars[1..].iter().collect::<String>();
+// res.push_str(&sub);
+// res.push(' ');
 
 pub fn change_case(input: &str) -> String {
     let mut res : String = "".to_string();
