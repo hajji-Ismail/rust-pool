d8cd911527d83c678e600503968a3abfdf870348
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-11 11:30:00 +0100

    with options

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -2,7 +2,10 @@ use std::path::Path;
 use std::fs::File;
  use std::io::Write;
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-  let mut file = File::create(path).unwrap();
+  let mut file = OpenOptions::new().append(true).read(true)
+            .write(true)
+            .create(true)
+            .open(path);
    let _ =file.write(content.as_bytes());
 
 }
