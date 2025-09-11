b0a7daa121092569814159c511994f9f6ebfdb54
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-11 10:48:13 +0100

    learning ok err methods

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -14,11 +14,11 @@ pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> Strin
   match(  server , security_level ) {
     (Ok(e) , _) =>  e.to_string(),
     (Err(e),Security::Unknown ) => format!("Unknown: {}", e),
-    (Err(e),Security::Message ) => format!("Message: {}", e),
+    (Err(e),Security::Message ) => panic!("Message: {}", e),
     (Err(e),Security::Warning ) =>  format!("Warning: {}", e),
     (Err(e),Security::NotFound ) =>  format!("NotFound: {}", e),
     (Err(e),Security::UnexpectedUrl ) =>  format!("UnexpectedUrl: {}", e),
-    (Err(""),Security::Message ) => format!("Unknown:EROR: Programe Stops", ),
+    (Err(""),Security::Message ) => panic!("Unknown:EROR: Programe Stops", ),
       
   }
 }
