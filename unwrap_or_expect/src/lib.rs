ac729e165cba2ebb214c446f401ba1ba5c4eda5d
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-11 10:46:51 +0100

    learning ok err methods

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,24 @@
+use std::fmt::format;
+
+ #[derive(Debug)]
+pub enum Security {
+    Unknown,
+    Message,
+    Warning,
+    NotFound,
+    UnexpectedUrl,
+}
+
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    // todo!()
+  match(  server , security_level ) {
+    (Ok(e) , _) =>  e.to_string(),
+    (Err(e),Security::Unknown ) => format!("Unknown: {}", e),
+    (Err(e),Security::Message ) => format!("Message: {}", e),
+    (Err(e),Security::Warning ) =>  format!("Warning: {}", e),
+    (Err(e),Security::NotFound ) =>  format!("NotFound: {}", e),
+    (Err(e),Security::UnexpectedUrl ) =>  format!("UnexpectedUrl: {}", e),
+    (Err(""),Security::Message ) => format!("Unknown:EROR: Programe Stops", ),
+      
+  }
+}
