6491e58bb47576a4425b6005319eb439807b055e
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-11 15:39:00 +0100

    learning the cipher

---
+++ cipher/src/lib.rs
@@ -0,0 +1,29 @@
+
+
+#[derive(Debug, PartialEq)]
+pub struct CipherError {
+    expected : String
+}
+
+pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
+
+let ciph = original.chars().map(|c|{
+  match c {
+        'a'..='z' => (b'z' - (c as u8 - b'a')) as char,
+        'A'..='Z' => (b'Z' - (c as u8 - b'A')) as char,
+        _ => c,
+    }
+}).collect::<String>();
+
+if ciph == ciphered.to_string() {
+    return Ok(());
+}
+Err(CipherError { expected: ciph })
+
+
+
+
+
+}
+
+
