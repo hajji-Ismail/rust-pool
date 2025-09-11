915be760293417ccf9bcfd7f7b82ba3796bd405b
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-11 11:23:36 +0100

    handling

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -3,6 +3,6 @@ use std::fs::File;
  use std::io::Write;
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
   let mut file = File::create(path).unwrap();
-   file.write(content.as_bytes());
+   let _ =file.write(content.as_bytes());
 
 }
