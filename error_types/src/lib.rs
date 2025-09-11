6a3ab600449cd9a2b975d3871c46d73b1c9e9cae
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-11 17:26:20 +0100

    errors with life time

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -2,14 +2,14 @@
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct FormError {
-    pub form_values: (String, String),
+    pub form_values: (& 'static str, String),
     pub date: String,
-    pub err: String,
+    pub err: & 'static str,
 }
 
 impl FormError {
     pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
-        FormError{form_values :(field_name.to_string(), field_value) , err: err.to_string() , date : chrono::Local::now().format("%Y-%m-%d %H:%M:%S").to_string()}
+        FormError{form_values :(field_name, field_value) , err: err , date : chrono::Local::now().format("%Y-%m-%d %H:%M:%S").to_string()}
       
     }
 }
@@ -25,15 +25,15 @@ impl Form {
         let data = chrono::Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
         if self.name.len() == 0 {
             return Err(FormError {
-                err: "Username is empty".to_string(),
-                form_values: ("name".to_string(), self.name.to_string()),
+                err: "Username is empty",
+                form_values: ("name", self.name.to_string()),
                 date:data,
             });
         }
         if self.password.len() < 8 {
             return Err(FormError {
-                err: "Password should be at least 8 characters long".to_string(),
-                form_values: ("password".to_string(), self.password.to_string()),
+                err: "Password should be at least 8 characters long",
+                form_values: ("password", self.password.to_string()),
                 date:data,
             });
         }
@@ -48,8 +48,8 @@ impl Form {
         });
 if !(digit&&letter&& symbol){
         return Err(FormError {
-                err: "Password should be a combination of ASCII numbers, letters and symbols".to_string(),
-                form_values: ("password".to_string(), self.password.to_string()),
+                err: "Password should be a combination of ASCII numbers, letters and symbols",
+                form_values: ("password", self.password.to_string()),
                 date: data,
             });
 }
