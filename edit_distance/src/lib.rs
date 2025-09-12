8d5c24064e604e9774acf05a7c4a3ebe9d8a21ef
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-12 18:45:29 +0100

    last push

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -1,47 +1,30 @@
 pub fn edit_distance(source: &str, target: &str) -> usize {
+    let m = source.len();
+    let n = target.len();
 
+    let mut dp = vec![vec![0; n + 1]; m + 1];
 
-    // let detect if one of them is epty 
-    if source.is_empty() {
-        return target.len()
+    for i in 0..=m {
+        dp[i][0] = i;
     }
-    if target.is_empty(){
-        return source.len()
-    }
-
-    let s =  source.len();
-    let t =target.len();
-
-
-    let s1 : Vec<char> = source.chars().collect();
-    let t1 : Vec<char> = target.chars().collect();
-    // println!("{:?}  {:?}" , s1 , t1);
-
-    let mut  table = vec![vec![0;t+1] ;s+1];
-    for i in 0..=s {
-        table[i][0] = i;
-    }
-     for j in 0..=t {
-        table[0][j] = j;
+    for j in 0..=n {
+        dp[0][j] = j;
     }
 
-    // lets fill the table herae 
-    for i in 1..=s {
-        for j in 1..=t {
-            // conpare chrcter 
-            if s1[i-1] == t1[j-1]{
-                table[i][j] = table[i-1][j-1];
-            }else {
-                table[i][j] = 1 + std::cmp::min(
-                    table[i-1][j-1],
-                    std::cmp::min(table[i-1][j],table[i][j-1])
-                )
-            }
+    for i in 1..=m {
+        for j in 1..=n {
+            let cost = if source.as_bytes()[i - 1] == target.as_bytes()[j - 1] { 
+                0 
+            } else { 
+                1 
+            };
+
+            dp[i][j] = *[dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + cost]
+                .iter()
+                .min()
+                .unwrap();
         }
     }
 
-
-   
-
-    table[s][t]
+    dp[m][n]
 }
