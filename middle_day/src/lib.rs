42fb14a95adc1519118711d359eb462d1949d3bd
hajji_Ismail <hajji.ismail.851@gmail.com>
2025-09-12 10:48:32 +0100

    better understanding of the subject

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -3,9 +3,10 @@ use chrono::{NaiveDate, Weekday, Datelike};
 pub fn middle_day(year: i32) -> Option<Weekday> {
 
     let is_leap = NaiveDate::from_ymd_opt(year, 2, 29).is_some();
-    let days = if is_leap { 366 } else { 365 };
+    let days = if is_leap { return  None;
+    } else { 365 };
 
-    let mid_day = days / 2 + 1;
+    let mid_day = days / 2+1 ;
 
 
     NaiveDate::from_yo_opt(year, mid_day).map(|d| d.weekday())
