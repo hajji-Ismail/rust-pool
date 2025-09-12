e2a0825422c19c42ab2d7c6e2ed2e25aff6211cf
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-12 18:42:27 +0100

    the error was a calcuel err

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -1,15 +1,14 @@
-use convert_case::{Case, Casing};
 use edit_distance::edit_distance;
+use convert_case::{Case, Casing};
 
 pub fn expected_variable(str1: &str, str2: &str) -> Option<String> {
     if str1.to_lowercase() == str1.to_lowercase().to_case(Case::Camel) || str1.to_lowercase()==str1.to_lowercase().to_case(Case::Snake){
        
 
         let steps = edit_distance(&str1.to_lowercase(), & str2.to_ascii_lowercase());
-        let persent = ((str2.len() - steps) as f64 / str2.len() as f64) * 100.0;
-
-        if persent > 50.0 {
-             Some(format!("{}%", (persent).round() ))
+ let percentage=((str2.len() as f64 -steps as f64)*100.0)/str2.len() as f64;
+        if percentage > 50.0 {
+             Some(format!("{}%", (percentage).round() ))
         } else {
             None
         }
