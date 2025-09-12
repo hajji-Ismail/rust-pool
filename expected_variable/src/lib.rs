af65f262b8d2cd8226e3fdfb1c6ec12eaf6550e8
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-12 18:35:32 +0100

    testing

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -1,30 +1,21 @@
-
-
-
-
 use convert_case::{Case, Casing};
 use edit_distance::edit_distance;
 
-pub fn expected_variable(str1 :&str , str2 :&str ) -> Option<String> {
+pub fn expected_variable(str1: &str, str2: &str) -> Option<String> {
+    if str1.to_lowercase() == str1.to_lowercase().to_case(Case::Camel) || str1.to_lowercase()==str1.to_lowercase().to_case(Case::Snake){
+       
 
-    if str1.to_ascii_lowercase() != str1.to_case(Case::Snake).to_ascii_lowercase()
-    && str1.to_ascii_lowercase() != str1.to_case(Case::Camel).to_ascii_lowercase(){
-        return None;
+        let steps = edit_distance(&str1.to_lowercase(), & str2.to_ascii_lowercase());
+        let persent = ((str2.len() - steps) as f64 / str2.len() as f64) * 100.0;
+
+        if persent > 50.0 {
+             Some(format!("{}%", (persent).round() ))
+        } else {
+            None
+        }
+
+    
+    } else {
+         None
     }
-
-    let lowstr1 = str1.to_lowercase();
-    let lowstr2 = str2.to_ascii_lowercase();
-
-    let steps = edit_distance(&lowstr1,&lowstr2);
- let persent = ((str2.len() - steps) as f64 / str2.len() as f64) * 100.0;
-
-      
-    if persent < 50.0 {
-      return  None;
-    }
-
-
-     Some(format!("{}%",(persent).round() as i64))
-
-
 }
