0801e7f88b4d9c74b2be5b95fcc69a431bc05480
hajji_Ismail <hajji.ismail.851@gmail.com>
2025-09-12 11:51:56 +0100

    does it fit

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,28 +1,78 @@
-mod areas_volumes;
+pub enum GeometricalShapes {
+    Square,
+    Circle,
+    Rectangle,
+    Triangle,
+}
+
+pub enum GeometricalVolumes {
+    Cube,
+    Sphere,
+    Cone,
+    TriangularPyramid,
+    Parallelepiped,
+}
+
+pub(crate) fn square_area(side: usize) -> usize {
+    side.pow(2)
+}
+
+pub(crate) fn triangle_area(base: usize, height: usize) -> f64 {
+    (base as f64 * height as f64) / 2.0
+}
+
+pub(crate) fn circle_area(radius: usize) -> f64 {
+    std::f64::consts::PI * (radius.pow(2) as f64)
+}
+
+pub(crate) fn rectangle_area(side_a: usize, side_b: usize) -> usize {
+    side_a * side_b
+}
+
+pub(crate) fn cube_volume(side: usize) -> usize {
+    side.pow(3)
+}
+
+pub(crate) fn sphere_volume(radius: usize) -> f64 {
+    (4.0 / 3.0) * std::f64::consts::PI * (radius.pow(3) as f64)
+}
+
+pub(crate) fn triangular_pyramid_volume(base_area: f64, height: usize) -> f64 {
+    (base_area * height as f64) / 3.0
+}
+
+pub(crate) fn parallelepiped_volume(side_a: usize, side_b: usize, side_c: usize) -> usize {
+    side_a * side_b * side_c
+}
+
+pub(crate) fn cone_volume(base_radius: usize, height: usize) -> f64 {
+    (1.0 / 3.0) * std::f64::consts::PI * base_radius.pow(2) as f64 * height as f64
+}
+
 pub fn area_fit(
     (x, y): (usize, usize),
-    kind: areas_volumes::GeometricalShapes,
+    kind: GeometricalShapes,
     times: usize,
     (a, b): (usize, usize),
 ) -> bool {
-    let erea = (x*y) as f64 ; 
-    let mut res = 0.0 ;
+    let erea = x*y; 
+    let mut res = 0 ;
     match kind {
-        areas_volumes::GeometricalShapes::Circle => {
-           res = areas_volumes::circle_area(a);
+        GeometricalShapes::Circle => {
+           res = circle_area(a)  as usize;
 
         },
-        areas_volumes::GeometricalShapes::Square => {
-            res = (areas_volumes::square_area(a)) as f64 ;
+        GeometricalShapes::Square => {
+            res = square_area(a) ;
         },
-        areas_volumes::GeometricalShapes::Rectangle => {
-            res = areas_volumes::rectangle_area(a, b) as f64;
+        GeometricalShapes::Rectangle => {
+            res = rectangle_area(a, b) ;
         } ,
-        areas_volumes::GeometricalShapes::Triangle => {
-            res = areas_volumes::triangle_area(a, b) 
+        GeometricalShapes::Triangle => {
+            res = triangle_area(a, b) as usize;
         }
     }
-    if erea == res * times as f64 {
+    if erea >= res * times{
         return  true;
 
     }
@@ -31,33 +81,33 @@ false
 
 pub fn volume_fit(
     (x, y, z): (usize, usize, usize),
-    kind: areas_volumes::GeometricalVolumes,
+    kind: GeometricalVolumes,
     times: usize,
     (a, b, c): (usize, usize, usize),
 ) -> bool {
         let erea = (x*y*z) as f64 ; 
     let mut res = 0.0 ;
     match kind {
-        areas_volumes::GeometricalVolumes::TriangularPyramid => {
-          res =   areas_volumes::triangular_pyramid_volume(a as f64, b);
+        GeometricalVolumes::TriangularPyramid => {
+          res =   triangular_pyramid_volume(a as f64, b);
          
 
         },
-        areas_volumes::GeometricalVolumes::Cone => {
-            res = areas_volumes::cone_volume(a, b);
+        GeometricalVolumes::Cone => {
+            res = cone_volume(a, b);
         },
-        areas_volumes::GeometricalVolumes::Cube => {
-            res = (areas_volumes::cube_volume(a )) as f64
+        GeometricalVolumes::Cube => {
+            res = (cube_volume(a )) as f64
         },
-        areas_volumes::GeometricalVolumes::Parallelepiped => {
-            res =( areas_volumes::parallelepiped_volume(a, b, c)) as f64 ;
+        GeometricalVolumes::Parallelepiped => {
+            res =( parallelepiped_volume(a, b, c)) as f64 ;
         },
-        areas_volumes::GeometricalVolumes::Sphere => {
-            res = ( areas_volumes::sphere_volume(a)) as f64 ;
+        GeometricalVolumes::Sphere => {
+            res = ( sphere_volume(a)) as f64 ;
         }
     
     }
-    if erea == res * times as f64 {
+    if erea >= res * times as f64 {
         return  true;
 
     }
