671fadfc39b46ddb7e781097afa2a30c47bb5bc2
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-13 10:58:11 +0100

    pushing

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -2,7 +2,7 @@ pub fn is_pangram(s: &str) -> bool {
     let mut nmb = 0 ;
     let arr = ['a','b','c', 'd', 'e', 'f', 'j', 'h', 'i','g' ,'k' , 'l' , 'm' , 'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t' ,'u','v','w' ,'x', 'y', 'z'];
      for c in arr {
-        if s.contains(c) {
+        if s.to_ascii_lowercase().contains(c) {
             nmb+=1
         }
 
