97fe96702414e29ebfe85e748f6f6dc4edfa1030
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-13 10:56:58 +0100

    enhasing the func logig and make it dynamically

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,16 +1,16 @@
 pub fn is_pangram(s: &str) -> bool {
     let mut nmb = 0 ;
-     for c in s.chars() {
+    let arr = ['a','b','c', 'd', 'e', 'f', 'j', 'h', 'i','g' ,'k' , 'l' , 'm' , 'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t' ,'u','v','w' ,'x', 'y', 'z'];
+     for c in arr {
+        if s.contains(c) {
+            nmb+=1
+        }
 
-     let ch: char = c.to_ascii_lowercase();
-    match ch {
-        'a'|'b'|'c'| 'd'| 'e'| 'f'| 'j'| 'h'| 'i'|'g' |'k' | 'l' | 'm' | 'n' |'o' |'p' |'q' |'r' |'s' |'t' |'u'|'v'|'w' |'x'| 'y'| 'z'=> nmb+= 1 ,
-     
-       _ => nmb += 0,
-
-    }
+   
+    
    }
-   if nmb < 26 {
+   // arr.len() to make it dynamically
+   if nmb < arr.len() {
     false
    } else {
     true
