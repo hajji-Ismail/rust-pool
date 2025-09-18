c1328c56fb4aaf50cdffc8a626ff2665b83d4478
hajji-Ismail <Hajji.ismail.851@gmail.com>
2025-09-18 12:37:36 +0100

    box

---
+++ box_recursion/src/lib.rs
@@ -0,0 +1,60 @@
+#[derive(Debug, PartialEq, Clone)]
+pub enum Role {
+    CEO,
+    Manager,
+    Worker,
+}
+
+impl From<&str> for Role {
+    fn from(s: &str) -> Self {
+        match s {
+            "CEO" => Role::CEO,
+            "Manager" => Role::Manager,
+        
+            _ =>  Role::Worker
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct WorkEnvironment {
+    pub grade: Link,
+}
+
+pub type Link = Option<Box< Worker>>; // Complete type alias
+
+#[derive(Debug)]
+pub struct Worker {
+    pub role: Role,
+    pub name: String,
+    pub next: Link,
+}
+
+impl WorkEnvironment {
+    pub fn new() -> Self {
+      
+        WorkEnvironment { grade: None }
+    }
+
+    pub fn add_worker(&mut self, name: &str, role: &str) {
+        let mut new = Worker{ name: name.to_string(), role : Role::from(role) , next: self.grade.take()} ; 
+        self.grade = Some(Box::new(new));
+
+      
+    }
+
+    pub fn remove_worker(&mut self) -> Option<String> {
+        self.grade.take().map(|boxed| {
+            let worker = *boxed; 
+            self.grade = worker.next;  
+            worker.name
+
+        })
+
+    }
+
+    pub fn last_worker(&self) -> Option<(String, Role)> {
+        self.grade.as_ref().map(|w| (w.name.clone(),w.role.clone() ))
+     
+    }
+}
