be07c3cff43ca9bc8d55e1cee11956ad900d2962
hajji-Ismail <Hajji.ismail.851@gmail.com>
2025-09-19 11:53:47 +0100

    cell reference

--- drop_the_thread/src/lib.rs
+++ drop_the_thread/src/lib.rs
@@ -30,14 +30,17 @@ impl ThreadPool {
     }
 
     pub fn drop_thread(&self, id: usize) {
-        if !self.is_dropped(id) && let Some(state) = self.states.borrow_mut().get_mut(id) {
-            if !*state {
-                *state = true;
-                self.drops.set(self.drops.get() + 1);
-            }
-        } else {
+        if id >= self.states.borrow().len() {
             panic!("{} is already dropped", id);
         }
+
+        let mut states = self.states.borrow_mut();
+        if states[id] {
+            panic!("{} is already dropped", id);
+        }
+
+        states[id] = true;
+        self.drops.set(self.drops.get() + 1);
     }
 }
 #[derive(Debug)]
