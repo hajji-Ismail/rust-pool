99ff8fff2caaaf271f5c9ab82c448483e34659da
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-09-27 16:51:08 +0100

    motion

--- project_motion/src/lib.rs
+++ project_motion/src/lib.rs
@@ -31,15 +31,26 @@ impl Iterator for ThrowObject {
         let dt = 1.0;
         let g = 9.8;
 
-        // Update velocities
         let new_vx = self.actual_velocity.x;
         let new_vy = self.actual_velocity.y - g * dt;
 
-        let new_x = self.actual_position.x + new_vx * dt; 
+        let new_x = self.actual_position.x + new_vx * dt;
         let new_y = self.actual_position.y + self.actual_velocity.y * dt - 0.5 * g * dt * dt;
 
-        // Update time
+        if new_y < 0.0 {
+            return None; 
+        }
+
         let new_time = self.time + dt;
+        fn round_to_decimals(value: f32, decimals: u32) -> f32 {
+            let factor = (10_f32).powi(decimals as i32);
+            (value * factor).round() / factor
+        }
+
+        let new_x = round_to_decimals(new_x, 4);
+        let new_y = round_to_decimals(new_y, 4);
+        let new_vx = round_to_decimals(new_vx, 4);
+        let new_vy = round_to_decimals(new_vy, 4);
 
         let new_throw = ThrowObject {
             init_position: self.init_position.clone(),
@@ -49,7 +60,6 @@ impl Iterator for ThrowObject {
             time: new_time,
         };
 
-        // Update self to new state
         *self = new_throw.clone();
 
         Some(new_throw)
